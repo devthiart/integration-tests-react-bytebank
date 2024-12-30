c369a742573c1692f6f99a1b7507bc1f2da08c52
Thiago Nunes <devthiart@gmail.com>
2024-12-30 15:49:24 -0300

    Controlling route history

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,11 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
 import App from './paginas/Principal/App';
-import { BrowserRouter } from 'react-router-dom';
+import Cartoes from './componentes/Cartoes';
 
 describe('Routes', () => {
   test('Must render the main route', () => {
     render(<App />, {wrapper: BrowserRouter}); // Render App with BrowserRouter.
     const user = screen.getByText('Olá, Joana :)!');
     expect(user).toBeInTheDocument();
+  });
+
+  test('Must render card routes', () => {
+    const initialRoute = '/cartoes';
+    render(
+      <MemoryRouter initialEntries={[initialRoute]}>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='cartoes' element={<Cartoes />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const myCards = screen.getByText('Meus cartões');
+    expect(myCards).toHaveTextContent('Meus cartões');
   });
 });
