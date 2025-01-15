e874b4ae7adccdf7be7ddf7cfd5a57829779bc56
Thiago Nunes <devthiart@gmail.com>
2025-01-15 18:09:24 -0300

    Challenge: Creating tests for bank balance

--- src/services/integrations.test.js
+++ src/services/integrations.test.js
@@ -1,17 +1,23 @@
 import api from './api';
 import { buscaTransacoes } from './transacoes';
+import { buscaSaldo } from './saldo';
 
 jest.mock('./api');
 
-const mockTransactions = [
-  {
-    id: 1,
-    transacao: 'Depósito',
-    valor: '100',
-    data: '22/11/2022',
-    mes: 'Novembro',
-  },
-];
+const mockData = {
+  "transacoes": [
+    {
+      id: 1,
+      transacao: 'Depósito',
+      valor: '100',
+      data: '22/11/2022',
+      mes: 'Novembro',
+    },
+  ],
+  "saldo": {
+    valor: 2000
+  }
+}
 
 const mockRequest = (response) => {
   return new Promise ((resolve) => {
@@ -31,19 +37,38 @@ const mockErrorRequest = () => {
 
 describe('Requests to API', () => {
   test('Must render a transaction list.', async () => {
-    api.get.mockImplementation(() => mockRequest(mockTransactions));
+    api.get.mockImplementation(() => mockRequest(mockData.transacoes));
 
     const transactions = await buscaTransacoes();
-    expect(transactions).toEqual(mockTransactions);
+    expect(transactions).toEqual(mockData.transacoes);
     expect(api.get).toHaveBeenCalledWith('/transacoes');
   });
 
-  test('Must render a empty when the request fails.', async () => {
+  test('Must render a empty list when the request fails.', async () => {
     api.get.mockImplementation(() => mockErrorRequest());
 
     const transactions = await buscaTransacoes();
 
     expect(transactions).toEqual([]);
     expect(api.get).toHaveBeenCalledWith('/transacoes');
+  });
+
+  test('Must render the balance value.', async () => {
+    api.get.mockImplementation(() => mockRequest(mockData.saldo));
+
+    const balance = await buscaSaldo();
+
+    expect(balance).toEqual(mockData.saldo.valor);
+    expect(api.get).toHaveBeenCalledWith('/saldo');
+  });
+
+  test('Must render a empty balance value when the request fails.', async () => {
+    api.get.mockImplementation(mockErrorRequest());
+
+    const balance = await buscaSaldo();
+
+    // In case of error, buscaSaldo() return the value 1000
+    expect(balance).toEqual(1000);
+    expect(api.get).toHaveBeenCalledWith('/saldo');
   });
 });
